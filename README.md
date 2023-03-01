aeb6a5f4cead16fb09f3c248d6a629e9a1e64082
naturallyanton <naturallyanton@gmail.com>
2023-03-01 16:11:14 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tokens-studio-style-dictionary-integration-example
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-x8zcgw)
---
+++ index.js
@@ -0,0 +1,39 @@
+const StyleDictionary = require('style-dictionary').extend({
+  source: ['output.tokens.json'],
+  platforms: {
+    css: {
+      transforms: [
+        'attribute/cti',
+        'name/cti/kebab',
+        'time/seconds',
+        'content/icon',
+        'size/rem',
+        'color/css',
+        'typography/shorthand',
+      ],
+      buildPath: 'build/',
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+        },
+      ],
+      options: {
+        outputReferences: true,
+      },
+    },
+  },
+});
+
+StyleDictionary.registerTransform({
+  name: 'typography/shorthand',
+  type: 'value',
+  transitive: true,
+  matcher: ({ type }) => type === 'typography',
+  transformer: ({ value }) => {
+    const { fontWeight, fontSize, lineHeight = 'normal', fontFamily } = value;
+    return `${fontWeight} ${fontSize}/${lineHeight} ${fontFamily}`;
+  },
+});
+
+StyleDictionary.buildAllPlatforms();
